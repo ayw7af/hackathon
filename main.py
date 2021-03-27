5869bb3884a502b4b159898e11ad3e48f60067b3
Chloe <tran.m.chloe@gmail.com>
2021-03-27 17:03:34 -0400

    add initial style

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -18,14 +13,24 @@ server = app.server
 fig = px.bar(da.df_sub, x="age", y="amount", color="category", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children=
-    'Fraud Classification Dashboard'
+    html.Header(
+        className="header",
+        children=[
+            html.H1('Fraud Classification', className="title")
+        ]
     ),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    html.P('This application showcases a classification of fraudulent charges based on a set of features.', className="description"), 
+    html.Div(
+        className="graphs-container", 
+        children=[
+            dcc.Graph(
+                id='graph1',
+                figure=fig
+            ),
+        ]
+    ),
+    html.P('Created by Amy Wang and Chloe Tran, 2020.', className="footer-tag"),
+    html.P('Powered with Dash.', className="footer-tag")
 ])
 
 if __name__ == '__main__':
